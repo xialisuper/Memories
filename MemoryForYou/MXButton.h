591a2d3ce9a7da742a86c0c7547637d4f174f283
xialisuper <xialisuper@163.com>
2017-09-26 00:21:02 +0800

    .......好像可以了?

---
+++ MemoryForYou/MXButton.h
@@ -0,0 +1,13 @@
+//
+//  MXButton.h
+//  MemoryForYou
+//
+//  Created by 夏立群 on 2017/9/26.
+//  Copyright © 2017年 xialisuper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXButton : UIButton
+
+@end
---
+++ MemoryForYou/MXButton.m
@@ -0,0 +1,21 @@
+//
+//  MXButton.m
+//  MemoryForYou
+//
+//  Created by 夏立群 on 2017/9/26.
+//  Copyright © 2017年 xialisuper. All rights reserved.
+//
+
+#import "MXButton.h"
+
+@implementation MXButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
