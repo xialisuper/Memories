652cd0c8720c00320f5b574acc58d9c1b86a67da
xialisuper <xialisuper@163.com>
2017-09-26 00:32:01 +0800

    ..

--- MemoryForYou/MXButton.m
+++ MemoryForYou/MXButton.m
@@ -10,12 +10,6 @@
 
 @implementation MXButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+//这是一段测试代码
 
 @end
