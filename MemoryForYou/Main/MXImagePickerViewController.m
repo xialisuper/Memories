60637ad8e995f11b3475adb5ae03d2106cafa293
xialisuper <xialisuper@163.com>
2017-11-08 10:27:19 +0800

    系统版本从9.0开始适配 增加了图片选择控制器的长按移动collectionView功能.

--- MemoryForYou/Main/MXImagePickerViewController.m
+++ MemoryForYou/Main/MXImagePickerViewController.m
@@ -12,10 +12,13 @@
 #import "MXPhotoUtil.h"
 
 static NSString * const kImagePickerCollectionViewCell = @"kImagePickerCollectionViewCell";
-@interface MXImagePickerViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
+@interface MXImagePickerViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UIGestureRecognizerDelegate>
 
 @property(nonatomic, strong) UICollectionView *photoCollectionView;
-@property (nonatomic, strong) NSArray<MXImageModel *> *dataArray;
+@property (nonatomic, strong) NSMutableArray<MXImageModel *> *dataArray;
+
+//长按移动手势
+@property(nonatomic, strong) UILongPressGestureRecognizer *longPressGesture;
 @end
 
 @implementation MXImagePickerViewController
@@ -26,6 +29,7 @@ static NSString * const kImagePickerCollectionViewCell = @"kImagePickerCollectio
     [self initDataSource];
     [self initNavBar];
     [self initPhotoCollectionView];
+    [self initGestureRecognizers];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -58,7 +62,7 @@ static NSString * const kImagePickerCollectionViewCell = @"kImagePickerCollectio
 - (void)initDataSource {
     [[MXPhotoUtil sharedInstance] fetchAllPhotosWithResultsWithBlock:^(NSArray<MXImageModel *> *assetsArray) {
         if (assetsArray.count) {
-            self.dataArray = assetsArray;
+            self.dataArray = [NSMutableArray arrayWithArray:assetsArray];
         }
     }];
 }
@@ -79,13 +83,91 @@ static NSString * const kImagePickerCollectionViewCell = @"kImagePickerCollectio
 }
 
 #pragma mark - UICollectionViewDelegate
-- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     NSLog(@"您点击了item:%ld", (long)indexPath.row);
     [collectionView cellForItemAtIndexPath:indexPath].backgroundColor = [UIColor redColor];
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canMoveItemAtIndexPath:(NSIndexPath *)indexPath{
+    //返回YES允许其item移动
+    return YES;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView moveItemAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath*)destinationIndexPath {
+    //取出源item数据
+    id objc = [self.dataArray objectAtIndex:sourceIndexPath.item];
+    //从资源数组中移除该数据
+    [self.dataArray removeObject:objc];
+    //将数据插入到资源数组中的目标位置上
+    [self.dataArray insertObject:objc atIndex:destinationIndexPath.item];
+}
+
+#pragma mark - getter & setter
+//- (UILongPressGestureRecognizer *)longPressGesture {
+//    if (_longPressGesture == nil) {
+//        _longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+//        _longPressGesture.minimumPressDuration = 0.3;
+//        _longPressGesture.delegate = self;
+//        [self.photoCollectionView addGestureRecognizer:_longPressGesture];
+//
+//    }
+//    return _longPressGesture;
+//}
+
+#pragma mark - method
+
+- (void)initGestureRecognizers {
+    if (self.photoCollectionView == nil) {
+        return;
+    }
+    
+    _longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+    _longPressGesture.minimumPressDuration = 0.3;
+    _longPressGesture.delegate = self;
+    [self.photoCollectionView addGestureRecognizer:_longPressGesture];
     
 }
 
+- (void)handleLongPress:(UILongPressGestureRecognizer *)longPressGesture {
+    //判断手势状态
+    switch (longPressGesture.state) {
+        case UIGestureRecognizerStateBegan:{
+            //判断手势落点位置是否在路径上
+            NSIndexPath *indexPath = [self.photoCollectionView indexPathForItemAtPoint:[longPressGesture locationInView:self.photoCollectionView]];
+            if (indexPath == nil) {
+                break;
+            }
+            //在路径上则开始移动该路径上的cell
+            [self.photoCollectionView beginInteractiveMovementForItemAtIndexPath:indexPath];
+        }
+            break;
+        case UIGestureRecognizerStateChanged:
+            //移动过程当中随时更新cell位置
 
+            if (@available(iOS 9.0, *)) {
+                [self.photoCollectionView updateInteractiveMovementTargetPosition:[longPressGesture locationInView:self.photoCollectionView]];
+            } else {
+                // Fallback on earlier versions
+            }
+            break;
+        case UIGestureRecognizerStateEnded:
+            //移动结束后关闭cell移动
+            if (@available(iOS 9.0, *)) {
+                [self.photoCollectionView endInteractiveMovement];
+            } else {
+                // Fallback on earlier versions
+            }
+            break;
+        default:
+            if (@available(iOS 9.0, *)) {
+                [self.photoCollectionView cancelInteractiveMovement];
+            } else {
+                // Fallback on earlier versions
+            }
+            break;
+    }
+}
+
+#pragma mark - UIGestureRecognizerDelegate
 
 @end
